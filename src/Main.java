8260671c231a2bac796b18a8002889391bd56839
Strochini <0.618@bk.ru>
2022-11-16 04:19:56 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Task 1
+        // Task 1 & 2
 
+        int month = 29000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i ++) {
+            total = total + total / 100;
+            total = total + month;
+            System.out.println("Месяц " + i + " Сумма накоплений равна " + total + " рублей");
+        }
 
     }
 }
